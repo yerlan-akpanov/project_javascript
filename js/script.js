9e04922e343e45cb4966030fea174ddb2d52aa3d
Mr.Robot <example@gmail.com>
2020-12-22 12:08:56 +0600

    second

--- js/script.js
+++ js/script.js
@@ -26,3 +26,23 @@
 // alert(`Hello ${user}`);
 
 // *******************************************************************************************
+
+const numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false
+};
+
+const a = prompt("Один из послдених просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
